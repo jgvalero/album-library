9c0e88168b593dd70db82104c8b38da0626cb009
Jose Valero <jvalero000@gmail.com>
2023-12-17 20:48:41 -0700

    Add Album component and sample album data

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,19 @@
+import Album from "./components/Album";
+import sampleAlbums from "./sampleAlbums.json";
+
 function Home() {
+  console.log(sampleAlbums);
   return (
     <div>
       <h1>Album Library</h1>
+      {sampleAlbums.map((album) => (
+        <Album
+          key={album.id}
+          albumArt={album.albumArt}
+          title={album.title}
+          artist={album.artist}
+        />
+      ))}
     </div>
   );
 }
---
+++ client/src/components/Album.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface AlbumProps {
+  albumArt: string;
+  title: string;
+  artist: string;
+}
+
+const Album: React.FC<AlbumProps> = ({ albumArt, title, artist }) => {
+  return (
+    <div>
+      <img src={albumArt} alt={`${title} album cover`} />
+      <h2>{title}</h2>
+      <p>{artist}</p>
+    </div>
+  );
+};
+
+export default Album;
